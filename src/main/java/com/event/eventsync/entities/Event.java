5df97610ad147b6f69baea6a84ab15947e05f1fc
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-11-12 17:22:03 +0200

    Added a data jpa dependency for JPA annotations. Also added and set up an Event entity.

---
+++ src/main/java/com/event/eventsync/entities/Event.java
@@ -0,0 +1,26 @@
+package com.event.eventsync.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "event")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+}
