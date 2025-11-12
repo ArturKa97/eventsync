c40f1f3819f0c07694ef7cc1b3b53816c328014e
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-11-12 17:59:59 +0200

    Added an EventRepository and injected it to EventService

--- src/main/java/com/event/eventsync/services/EventService.java
+++ src/main/java/com/event/eventsync/services/EventService.java
@@ -1,8 +1,13 @@
 package com.event.eventsync.services;
 
+import com.event.eventsync.repositories.EventRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EventService {
+
+    private final EventRepository eventRepository;
 
 }
