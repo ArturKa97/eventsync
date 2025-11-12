dd1b92ed2dd1e055425d372d3c5a207308e01e9f
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-11-12 20:06:01 +0200

    Added and set up an EventFeedback entity;

---
+++ src/main/java/com/event/eventsync/entities/EventFeedback.java
@@ -0,0 +1,32 @@
+package com.event.eventsync.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "event_feedback")
+public class EventFeedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "feedback")
+    private String feedback;
+
+    @Column(name = "time_stamp")
+    @CreationTimestamp  // Sets the date to .now() when the entity is first created
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timeStamp;
+
+}
