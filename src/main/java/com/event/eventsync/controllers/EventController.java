2fb816d65ecf968a49236fc96b2b3ae386b65e84
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-11-12 17:29:09 +0200

    Added an EventService and injected it into the EventController

--- src/main/java/com/event/eventsync/controllers/EventController.java
+++ src/main/java/com/event/eventsync/controllers/EventController.java
@@ -1,10 +1,15 @@
 package com.event.eventsync.controllers;
 
+import com.event.eventsync.services.EventService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/event")
+@RequiredArgsConstructor
 public class EventController {
+
+    private final EventService eventService;
 
 }
---
+++ src/main/java/com/event/eventsync/services/EventService.java
@@ -0,0 +1,8 @@
+package com.event.eventsync.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+}
