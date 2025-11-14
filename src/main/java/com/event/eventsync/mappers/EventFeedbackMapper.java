1fb938bbd38fbf77beafbaf9ada3d1f838f3a651
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-11-14 21:46:49 +0200

    Adding all needed properties to EventFeedbackDTO and specifying the mappers to work correctly and now the Event structure is correct when fetching all events.

--- src/main/java/com/event/eventsync/mappers/EventFeedbackMapper.java
+++ src/main/java/com/event/eventsync/mappers/EventFeedbackMapper.java
@@ -3,11 +3,14 @@ package com.event.eventsync.mappers;
 import com.event.eventsync.dtos.EventFeedbackDTO;
 import com.event.eventsync.entities.EventFeedback;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {EventSentimentMapper.class})
 public interface EventFeedbackMapper {
 
+    @Mapping(target = "eventSentimentDTO", source = "eventFeedback.sentiment")
     EventFeedbackDTO toDTO(EventFeedback eventFeedback);
+    @Mapping(target = "sentiment", source = "eventFeedbackDTO.eventSentimentDTO")
     EventFeedback toEntity(EventFeedbackDTO eventFeedbackDTO);
 
 }
--- src/main/java/com/event/eventsync/mappers/EventMapper.java
+++ src/main/java/com/event/eventsync/mappers/EventMapper.java
@@ -3,11 +3,14 @@ package com.event.eventsync.mappers;
 import com.event.eventsync.dtos.EventDTO;
 import com.event.eventsync.entities.Event;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {EventFeedbackMapper.class})
 public interface EventMapper {
 
+    @Mapping(target = "eventFeedbackList", source = "event.eventFeedbackList")
     EventDTO toDTO(Event event);
+    @Mapping(target = "eventFeedbackList", source = "eventDTO.eventFeedbackList")
     Event toEntity(EventDTO eventDTO);
 
 }
