bf3134d2c7965e75cd306730997784fb9bff9155
ArturKa97 <115172506+ArturKa97@users.noreply.github.com>
2025-11-16 14:05:54 +0200

    Adding MUI icons and attached them to the reviews accordingly. Also added some styling to the EventInfo main part of title, description and the review summary.

--- eventsync-ui/src/components/EventInfo.jsx
+++ eventsync-ui/src/components/EventInfo.jsx
@@ -3,7 +3,17 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { getEventById, getEventSummary } from "../api/EventApi";
 import EventFeedbackForm from "../forms/EventFeedbackForm";
-import { MainContainer } from "../styles/StyledComponents";
+import {
+  EventInfoBox,
+  EventMainBox,
+  EventSummaryBox,
+  MainContainerColumn,
+  StyledDescriptionTypography,
+  StyledTitleTypography,
+} from "../styles/StyledComponents";
+import SentimentNeutralIcon from "@mui/icons-material/SentimentNeutral";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
 
 function EventInfo() {
   const [eventInfo, setEventInfo] = useState({});
@@ -11,6 +21,11 @@ function EventInfo() {
   const location = useLocation(); //getting the id passed down from clicking the EventCard
 
   const sentimentOrder = ["Positive", "Neutral", "Negative"];
+  const sentimentIcons = {
+    Positive: <SentimentSatisfiedAltIcon sx={{ color: "green" }} />,
+    Neutral: <SentimentNeutralIcon sx={{ color: "yellow" }} />,
+    Negative: <SentimentVeryDissatisfiedIcon sx={{ color: "red" }} />,
+  };
 
   const fetchEvent = async () => {
     try {
@@ -36,14 +51,25 @@ function EventInfo() {
   }, []);
 
   return (
-    <>
-      <Typography>{eventInfo.title}</Typography>
-      <Typography>{eventInfo.description}</Typography>
-      {sentimentOrder.map((key) => (
-        <Typography key={key}>
-          {key}: {eventSummary[key]}
-        </Typography>
-      ))}
+    <MainContainerColumn>
+      <EventMainBox>
+        <EventInfoBox>
+          <StyledTitleTypography variant="h4">
+            {eventInfo.title}
+          </StyledTitleTypography>
+          <StyledDescriptionTypography variant="h6">
+            {eventInfo.description}
+          </StyledDescriptionTypography>
+        </EventInfoBox>
+        <EventSummaryBox>
+          <Typography variant="h5">Event Review Summary:</Typography>
+          {sentimentOrder.map((key) => (
+            <Typography variant="h6" key={key}>
+              {key}: {eventSummary[key]} {sentimentIcons[key]}
+            </Typography>
+          ))}
+        </EventSummaryBox>
+      </EventMainBox>
       <EventFeedbackForm eventId={eventInfo.id} />
       {eventInfo.eventFeedbackList?.map((eventFeedback, index) => (
         <div key={index}>
@@ -53,7 +79,7 @@ function EventInfo() {
           <Typography>{eventFeedback.eventSentimentDTO.score}</Typography>
         </div>
       ))}
-    </>
+    </MainContainerColumn>
   );
 }
 export default EventInfo;
--- eventsync-ui/src/styles/StyledComponents.jsx
+++ eventsync-ui/src/styles/StyledComponents.jsx
@@ -45,6 +45,14 @@ export const MainContainer = styled(Container)({
   padding: "4.8rem 0 9.6rem 0",
   margin: "0 auto",
 });
+export const MainContainerColumn = styled(Container)({
+  display: "flex",
+  flexDirection: "column",
+  flexWrap: "wrap",
+  maxWidth: "1200",
+  padding: "4.8rem 0 9.6rem 0",
+  margin: "0 auto",
+});
 
 export const StyledCard = styled(Card)({
   width: 270,
@@ -69,12 +77,15 @@ export const StyledTitleTypography = styled(Typography)({
   justifyContent: "center",
   whiteSpace: "normal",
   wordBreak: "break-word",
+  margin: "0rem 0rem 0rem 0.5rem",
+  borderBottom: "2px solid rgb(0, 0, 0)",
 });
 export const StyledDescriptionTypography = styled(Typography)({
   flex: 1,
   display: "flex",
   whiteSpace: "normal",
   wordBreak: "break-word",
+  margin: "0.5rem 0rem 0rem 0.5rem",
 });
 
 export const FormBox = styled(Box)({
@@ -93,4 +104,30 @@ export const FormActionButtonBox = styled(Box)({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+});
+
+export const EventInfoBox = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  padding: "1.0rem",
+  flex: 3,
+  margin: "1.0rem 0rem",
+});
+export const EventSummaryBox = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  padding: "1.0rem",
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "left",
+  margin: "1.0rem 0rem",
+  border: "2px solid rgb(0, 0, 0)",
+});
+
+export const EventMainBox = styled(Box)({
+  display: "flex",
+  flexDirection: "row",
+  padding: "1.0rem",
+  margin: "1.0rem 0rem",
+  borderBottom: "2px solid rgb(0, 0, 0)",
 });
